e32de140d594e2a85faef13f37ac674a7f6e4cf2
MAnxo7 <miguel_anxorl_05@hotmail.es>
2026-02-21 15:15:09 +0100

    Ahora las figs se guardan en una carpeta /runs/figs y tambien se muestra el mejor epoch al terminar el entrenamiento.

--- src/viz.py
+++ src/viz.py
@@ -1,9 +1,11 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import csv
+import os 
 
 def plot_from_csv(csv_path):
     epochs, train_loss, val_loss, train_acc, val_acc = [], [], [], [], []
-
+    savepath = os.path.split(csv_path)[0]
     with open(csv_path, newline='') as f:
         reader = csv.DictReader(f)
         for row in reader:
@@ -14,10 +16,10 @@ def plot_from_csv(csv_path):
                 val_loss.append(float(row['loss']))
                 val_acc.append(float(row['acc']))
                 epochs.append(int(row['epoch']))
-    visualize_loss(train_loss, val_loss)
-    visualize_acc(train_acc, val_acc)
+    visualize_loss(train_loss, val_loss, save_path=os.path.join(savepath,'figures','loss.jpg'))
+    visualize_acc(train_acc, val_acc, save_path=os.path.join(savepath,'figures','acc.jpg'))
     
-def visualize_loss(train_loss, val_loss):
+def visualize_loss(train_loss, val_loss, save_path=None):
     plt.figure(figsize=(7, 4))
     plt.plot(train_loss, label="Train Loss", linewidth=2)
     plt.plot(val_loss, label="Validation Loss", linewidth=2, linestyle="--")
@@ -27,10 +29,12 @@ def visualize_loss(train_loss, val_loss):
     plt.legend()
     plt.grid(True, linestyle="--", alpha=0.6)
     plt.tight_layout()
+    if(save_path):
+        plt.savefig(save_path)
     plt.show()
 
 
-def visualize_acc(train_acc, val_acc):
+def visualize_acc(train_acc, val_acc, save_path=None):
     plt.figure(figsize=(7, 4))
     plt.plot(train_acc, label="Train Accuracy", linewidth=2)
     plt.plot(val_acc, label="Validation Accuracy", linewidth=2, linestyle="--")
@@ -41,4 +45,6 @@ def visualize_acc(train_acc, val_acc):
     plt.legend()
     plt.grid(True, linestyle="--", alpha=0.6)
     plt.tight_layout()
+    if(save_path):
+        plt.savefig(save_path)
     plt.show()
